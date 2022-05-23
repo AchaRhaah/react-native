2720ca64ac8f6717d2c7cee807931f404dbb1376
Rha'ah <acharhaah2003@gmial.com>
2022-05-23 05:18:02 +0100

    good work!! Next style the inputs

--- BusProj/App.js
+++ BusProj/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SpecificationsForm from './screens/SpecificationsForm';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SpecificationsForm />
       <StatusBar style="auto" />
     </View>
   );
---
+++ BusProj/screens/SpecificationsForm.js
@@ -0,0 +1,23 @@
+import react from "react"
+import { TextInput, View, Text, StyleSheet } from "react-native";
+// import { Formik } from 'formik'
+
+const SpecificationsForm = () => {
+    return <View>
+
+        <Text>Book A Trip</Text>
+        <TextInput type="text" style={styles.input} placeholder="Place of departure" />
+        <TextInput type="text" style={styles.input} placeholder="Destination" />
+        <TextInput type="text" style={styles.input} placeholder="Date of journey" />
+        <TextInput type="text" style={styles.input} placeholder="Date of journey" />
+        <TextInput type="text" style={styles.input} placeholder="Time of journey" />
+
+
+    </View>
+}
+const styles = StyleSheet.create({
+    input: {
+        // borderColor: 'red'
+    }
+});
+export default SpecificationsForm
