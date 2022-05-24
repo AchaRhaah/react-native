43f843c66b62999fdc0b46fedc25ab5c449c7436
AchaRhaah <acharhaah2003@gmail.com>
2022-05-24 04:55:25 +0100

    todo app/Made with Matt

--- toDo/components/task.js
+++ toDo/components/task.js
@@ -3,10 +3,10 @@ import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 // import { TouchableOpacity } from "react-native";
 const Task = (props) =>{
     return (
-        <View style={styles.item}>
+        <View style={ styles.item }>
             <View style={styles.itemLeft}>
                 <View style={styles.square}></View>
-                <Text style={styles.itemText}>{props.text}</Text>
+                <Text style={styles.itemText}>{props.text}</Text >
             </View>
             <View style={styles.circular}></View>
         </View>
