41c4c4d05d1aab85054fd1086d8a105d3681a0a0
Rha'ah <acharhaah2003@gmial.com>
2022-06-23 17:25:03 +0100

    States Exercise

--- NetNinja/netNinja/App.js
+++ NetNinja/netNinja/App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [hairStyle, setAppointment] = useState({
+    name: "Knotless",
+    price: 6000,
+  });
+  const pressHandler = () => {
+    setAppointment({ name: "twist", price: 6000 });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        {hairStyle.name} cost {hairStyle.price}
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button title="Update State" onPress={pressHandler} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
