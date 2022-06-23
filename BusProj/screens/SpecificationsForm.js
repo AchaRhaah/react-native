17a199926b25e554ca888b3694a3a41fb155737f
AchaRhaah <acharhaah2003@gmail.com>
2022-06-23 01:50:39 +0100

    started the buss app

--- BusProj/screens/SpecificationsForm.js
+++ BusProj/screens/SpecificationsForm.js
@@ -1,21 +1,34 @@
-import react from "react"
+import react from "react";
+import { styles } from "./styles/formStyles";
 import { TextInput, View, Text, StyleSheet } from "react-native";
 // import { Formik } from 'formik'
 
 const SpecificationsForm = () => {
-    return <View>
-
-        <Text>Book A Trip</Text>
-        <TextInput type="text" style={styles.input} placeholder="Place of departure" />
-        <TextInput type="text" style={styles.input} placeholder="Destination" />
-        <TextInput type="text" style={styles.input} placeholder="Date of journey" />
-        <TextInput type="text" style={styles.input} placeholder="Date of journey" />
-        <TextInput type="text" style={styles.input} placeholder="Time of journey" />
+  return (
+    <View style={styles.container}>
+      <Text>Book A Trip</Text>
+      <TextInput
+        type="text"
+        style={styles.input}
+        placeholder="Leaving from..."
+      />
+      <TextInput type="text" style={styles.input} placeholder="Going to..." />
+      <TextInput
+        type="text"
+        style={styles.input}
+        placeholder="Date of journey"
+      />
+      <TextInput
+        type="text"
+        style={styles.input}
+        placeholder="Date of journey"
+      />
+      <TextInput
+        type="text"
+        style={styles.input}
+        placeholder="Time of journey"
+      />
     </View>
-}
-const styles = StyleSheet.create({
-    input: {
-        // borderColor: 'red'
-    }
-});
-export default SpecificationsForm
+  );
+};
+export default SpecificationsForm;
---
+++ BusProj/screens/styles/formStyles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+const styles = StyleSheet.create({
+  input: {
+    width: 298,
+    height: 49,
+    fontSize: 20,
+    backgroundColor: "white",
+    borderWidth: 2,
+    borderColor: "#c4c4c4",
+    borderRadius: 30,
+  },
+  container: {
+    justifyContent: "space-between",
+  },
+});
+
+export { styles };
