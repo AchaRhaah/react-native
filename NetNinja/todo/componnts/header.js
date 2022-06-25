9d52cec10fef695396ff34f165d42e6016041ef1
Rha'ah <acharhaah2003@gmial.com>
2022-06-25 07:21:31 +0100

    header component of todo app

---
+++ NetNinja/todo/componnts/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Todos</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headera: {
+    height: 80,
+    padding: 38,
+    backgroundColor: "coral",
+  },
+});
