f6206dae8547f7d94c2a33279de22c49845c392e
Rha'ah <acharhaah2003@gmial.com>
2022-06-25 07:22:38 +0100

    all good

--- NetNinja/todo/App.js
+++ NetNinja/todo/App.js
@@ -1,71 +1,36 @@
-import { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 export default function App() {
-  const [hairstyles, setAppointment] = useState([
-    { name: "Knotless", price: 6000, key: 1 },
-    { name: "twists", price: 6500, key: 2 },
-    { name: "Ghana Braids", price: 3500, key: 3 },
-    { name: "Jungle Braids", price: 7000, key: 4 },
-    { name: "butterfly Locs", price: 10000, key: 5 },
+  const [tasks, setTask] = useState([
+    { text: "attend google io", time: "10:30am", key: "1" },
+    { text: "read for resit", time: "1:30pm", key: "2" },
+    { text: "eat", time: "6:30pm", key: "4" },
+    { text: "call my family", time: "10:30pm", key: "3" },
   ]);
-  const pressHandler = (key) => {
-    setAppointment((prevAppt) => {
-      return prevAppt.filter((person) => person.key != key);
-    });
-  };
   return (
     <View style={styles.container}>
-      <FlatList
-        data={hairstyles}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.key)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      {/* {hairstyles.map((item) => {
-        return (hairstyles
-          <View key={item.key}>
-            <Text>{item.name}</Text>
-          </View>
-        );
-      })} */}
+      <View style={styles.content}>
+        <View style={styles.list}>
+          <FlatList
+            data={tasks}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  buttonContainer: {
+  content: {
+    padding: 40,
+  },
+  list: {
     marginTop: 20,
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#777",
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-  item: {
-    color: "red",
-    fontSize: 30,
-    width: 300,
-    height: 80,
-    margin: 50,
-    backgroundColor: "pink",
   },
 });
